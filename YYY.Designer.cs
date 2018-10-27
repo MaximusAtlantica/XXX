a0c60a844b70517846eaabdc9237c2f47c6ccc79
ChupUchup <untuk.software@gmail.com>
2018-10-27 23:26:40 +0700

    finalize commit

---
+++ YYY.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace XXX
+{
+    partial class YYY
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YYY));
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.num_delay = new System.Windows.Forms.NumericUpDown();
+            this.lbl_ms = new System.Windows.Forms.Label();
+            this.panel_utama = new System.Windows.Forms.Panel();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.check_pick = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.num_delay)).BeginInit();
+            this.panel_utama.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(130, -1);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(20, 20);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "X";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // num_delay
+            // 
+            this.num_delay.DecimalPlaces = 1;
+            this.num_delay.Enabled = false;
+            this.num_delay.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.num_delay.Location = new System.Drawing.Point(0, 0);
+            this.num_delay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.num_delay.Name = "num_delay";
+            this.num_delay.Size = new System.Drawing.Size(38, 20);
+            this.num_delay.TabIndex = 1;
+            this.num_delay.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.num_delay.ValueChanged += new System.EventHandler(this.num_delay_ValueChanged);
+            //this.num_delay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_utama_MouseDown);
+            // 
+            // lbl_ms
+            // 
+            this.lbl_ms.AutoSize = true;
+            this.lbl_ms.Enabled = false;
+            this.lbl_ms.Location = new System.Drawing.Point(37, 3);
+            this.lbl_ms.Name = "lbl_ms";
+            this.lbl_ms.Size = new System.Drawing.Size(25, 13);
+            this.lbl_ms.TabIndex = 1;
+            this.lbl_ms.Text = "dtk";
+            //this.lbl_ms.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_utama_MouseDown);
+            // 
+            // panel_utama
+            // 
+            this.panel_utama.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel_utama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_utama.Controls.Add(this.check_pick);
+            this.panel_utama.Controls.Add(this.num_delay);
+            this.panel_utama.Controls.Add(this.btn_exit);
+            this.panel_utama.Controls.Add(this.btn_help);
+            this.panel_utama.Controls.Add(this.lbl_ms);
+            this.panel_utama.Location = new System.Drawing.Point(0, 0);
+            this.panel_utama.Name = "panel_utama";
+            this.panel_utama.Size = new System.Drawing.Size(150, 20);
+            this.panel_utama.TabIndex = 5;
+            this.panel_utama.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_utama_MouseDown);
+            // 
+            // btn_help
+            // 
+            this.btn_help.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_help.Location = new System.Drawing.Point(112, -1);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(20, 20);
+            this.btn_help.TabIndex = 3;
+            this.btn_help.Text = "?";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
+            // 
+            // check_pick
+            // 
+            this.check_pick.AutoSize = true;
+            this.check_pick.Enabled = false;
+            this.check_pick.Location = new System.Drawing.Point(63, 2);
+            this.check_pick.Name = "check_pick";
+            this.check_pick.Size = new System.Drawing.Size(50, 17);
+            this.check_pick.TabIndex = 6;
+            this.check_pick.Text = "Pick";
+            this.check_pick.UseVisualStyleBackColor = true;
+            // 
+            // YYY
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(150, 23);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel_utama);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "YYY";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YYY";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.YYY_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.num_delay)).EndInit();
+            this.panel_utama.ResumeLayout(false);
+            this.panel_utama.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.NumericUpDown num_delay;
+        private System.Windows.Forms.Label lbl_ms;
+        private System.Windows.Forms.Panel panel_utama;
+        private System.Windows.Forms.Button btn_help;
+        private System.Windows.Forms.CheckBox check_pick;
+
+    }
+}
---
+++ YYY.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Gma.System.MouseKeyHook;
+using Microsoft.Win32;
+
+namespace XXX
+{
+    public partial class YYY : Form
+    {
+        #region variables
+        
+        uint
+            SW_SHOW = 0x0005,
+            WM_NCLBUTTONDOWN = 0x000000A1,
+            HT_CAPTION = 0x00000002,
+            WM_KEYDOWN = 0x00000100,
+            WM_CHAR = 0x00000102,
+            WM_KEYUP = 0x00000101,
+            w_down = 0x00000058,
+            w_ch = 0x00000078,
+            w_up = 0x00000058,
+            l_down = 0x002D0001,
+            l_ch = 0x002D0001,
+            l_up = 0xC02D0001;
+        Timer t1 = new Timer();
+        Timer t2 = new Timer();
+        IntPtr RFWindowHandle = IntPtr.Zero;
+        IKeyboardMouseEvents hook;
+                        
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle rect);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint uMsg, uint wParam, uint lParam);
+
+        [DllImport("User32.dll")]
+        public static extern int PostMessage(IntPtr hWnd, uint uMsg, uint wParam, uint lParam);
+
+        #endregion variables
+
+        public YYY()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(250, 1, 2, 3);
+            this.TransparencyKey = Color.FromArgb(250, 1, 2, 3);
+            this.AllowTransparency = true;
+            t1.Interval = Convert.ToInt32(num_delay.Value * (decimal)1000);
+            t1.Enabled = false;
+            t1.Tick += (s, e) => { dowork(RFWindowHandle); };
+            t2.Interval = 1000;
+            t2.Tick += (s, e) => { checkRFWindow(); };
+        }
+
+        private void YYY_Shown(object sender, EventArgs e)
+        {
+            checkRFWindow();
+            if (RFWindowHandle != IntPtr.Zero)
+            {
+                ShowWindow(RFWindowHandle, SW_SHOW);
+                SetForegroundWindow(RFWindowHandle);
+                Rectangle wRect = new Rectangle();
+                GetWindowRect(RFWindowHandle, out wRect);
+                this.Location = new Point(wRect.X, wRect.Y);
+            }
+            hook = Hook.GlobalEvents();
+            hook.KeyDown += hook_KeyDown;
+            t2.Enabled = true;
+            t2.Start();
+        }
+
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (GetForegroundWindow() == RFWindowHandle)
+            {
+                if (e.KeyCode == Keys.X)
+                {
+                    if (t1.Enabled)
+                    {
+                        t1.Enabled = false;
+                        t1.Stop();
+                        check_pick.Checked = false;
+                    }
+                    else
+                    {
+                        t1.Enabled = true;
+                        t1.Start();
+                        check_pick.Checked = true;
+                    }
+                }
+                else if (e.KeyCode == Keys.F11)
+                {
+                    if (num_delay.Value < num_delay.Maximum)
+                        num_delay.Value += (decimal)0.1;
+                }
+                else if (e.KeyCode == Keys.F12)
+                {
+                    if (num_delay.Value > num_delay.Minimum)
+                        num_delay.Value -= (decimal)0.1;
+                }
+            }
+        }
+
+        void dowork(IntPtr hwnd)
+        {
+            if (RFWindowHandle != IntPtr.Zero)
+            {
+                SendMessage(hwnd, WM_KEYDOWN, (uint)Keys.X, 0);
+                //SendMessage(hwnd, WM_CHAR, (uint)Keys.Z, l_ch);
+                //SendMessage(hwnd, WM_KEYUP, (uint)Keys.Z, l_up);
+
+                //SendMessage(hwnd, WM_KEYDOWN, (uint)Keys.Z, 0);
+
+                //PostMessage(hwnd, WM_KEYDOWN, (uint)Keys.X, 0);
+            }
+            else
+            {
+                t1.Enabled = false;
+                t1.Stop();
+                check_pick.Checked = false;
+            }
+        }
+
+        void checkRFWindow()
+        {
+            try
+            {
+                RFWindowHandle = Process.GetProcessesByName("RF_Online.bin").FirstOrDefault().MainWindowHandle;
+                panel_utama.BackColor = Color.FromArgb(192, 255, 192);
+            }
+            catch
+            {
+                RFWindowHandle = IntPtr.Zero;
+                panel_utama.BackColor = Color.FromArgb(255, 192, 255);
+            }
+        }
+
+        private void num_delay_ValueChanged(object sender, EventArgs e)
+        {
+            t1.Interval = Convert.ToInt32(num_delay.Value * 1000);
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel_utama_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            string message = 
+                "F11 = Delay ngepick bertambah 0.1 Detik\n"+
+                "F12 = Delay ngepick berkurang 0.1 Detik\n"+
+                "X = On/Off ngepick";
+            MessageBox.Show(message, "Informasi", MessageBoxButtons.OK);
+        }
+
+    }
+}
